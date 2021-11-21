363850ea1067f4e8b36e53362de41c777c757fe0
Dalen Ledingham <dalen.ledingham@gmail.com>
2021-11-21 08:52:08 -0600

    Initial attempt at endpoints

--- parserator_web/views.py
+++ parserator_web/views.py
@@ -16,9 +16,19 @@ class AddressParse(APIView):
     def get(self, request):
         # TODO: Flesh out this method to parse an address string using the
         # parse() method and return the parsed components to the frontend.
-        return Response({})
+        input_string = "123 main st chicago il"
+        parsed_address = AddressParse.parse(self, input_string)
+
+        return Response(parsed_address)
 
     def parse(self, address):
         # TODO: Implement this method to return the parsed components of a
         # given address using usaddress: https://github.com/datamade/usaddress
+        parsed_address = usaddress.tag(address)
+        address_components = []
+        address_type = parsed_address[1]
+
+        for component in parsed_address[0]:
+            address_components.append(component)
+
         return address_components, address_type
